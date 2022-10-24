1676e9fd92bc956a1c47e6e5603686ee74becf21
Javikeee <javikeee.jer@gmail.com>
2022-10-24 09:21:26 +0200

    cosas

--- Proyectos/T1/Ej1/JAXWS Calculadora/src/org/iesvi/ws/CalculadoraPusblish.java
+++ Proyectos/T1/Ej1/JAXWS Calculadora/src/org/iesvi/ws/CalculadoraPusblish.java
@@ -5,5 +5,6 @@ import javax.xml.ws.Endpoint;
 public class CalculadoraPusblish {
     public static void main(String[] args){
         Endpoint.publish("http://localhost:1516/WS/Calculadora", new CalculadoraImpl());
+        System.out.println("Servicio activo, esperando peticiones");
     }
 }
